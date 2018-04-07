4c9c863b1ca5c684f04d12063fba8999e5b78ac1
angelcom <179908080@qq.com>
2018-04-07 23:01:04 +0800

    release 1.0.0

--- main.ts
+++ main.ts
@@ -7,67 +7,154 @@
 /**
  * I2C LCM1602-14 液晶软件包
  */
-//% weight=100 color=#0fbc11 icon="/u2618"
-namespace I2C_LCD1602 {
+//% weight=100 color=#55F343 icon=""
+namespace I2C_LCD1602_14 {
     let i2cAddr: number // 0x3E
-    let BK: number      // backlight control
-    let RS: number      // command/data
+    //let BK: number      // backlight control Not Use
 
-    // send command
-    function cmd(d: number) {
-        pins.i2cWriteNumber(i2cAddr, 0x8000|d, 9)
-        //basic.pause(1)
-        //i2ccmd(i2cAddr,d)
-    }
-        // send data
-    function dat(d: number) {
-         pins.i2cWriteNumber(i2cAddr, 0x4000|d, 9)
-         //basic.pause(1)
-         //i2cwrite(i2cAddr,0x40,d)
-    }
-    // 从robotbit找来的代码，看上去更科学一些。。。。
-    function i2cwrite(addr: number, reg: number, value: number) {
-        let buf = pins.createBuffer(2)
-        buf[0] = reg
-        buf[1] = value
-        pins.i2cWriteBuffer(addr, buf)
+    export enum CustomChar {
+        //% block="Heart"
+        Heart = 0,
+        HeartSmall = 1,
+        //% block="Smile"
+        Smile = 2,
+        Sad = 3,
+        Degree = 4,
+        ArmsUp = 5,
+        ArmsDown = 6,
     }
 
-    function i2ccmd(addr: number, value: number) {
-        let buf = pins.createBuffer(1)
-        buf[0] = value
-        pins.i2cWriteBuffer(addr, buf)
+    let Heart = [
+        0b00000,
+        0b01010,
+        0b11111,
+        0b11111,
+        0b11111,
+        0b01110,
+        0b00100,
+        0b00000
+    ];
+
+    let HeartSmall = [
+        0b00000,
+        0,
+        0b01010,
+        0b01110,
+        0b01110,
+        0b00100,
+        0,
+        0
+    ];
+
+    let Smile = [
+        0b00000,
+        0b00000,
+        0b01010,
+        0b00000,
+        0b00000,
+        0b10001,
+        0b01110,
+        0b00000
+    ];
+
+    let Sad = [
+        0b00000,
+        0b00000,
+        0b01010,
+        0b00000,
+        0b00000,
+        0b00000,
+        0b01110,
+        0b10001
+    ];
+    let ArmsDown = [
+        0b00100,
+        0b01010,
+        0b00100,
+        0b00100,
+        0b01110,
+        0b10101,
+        0b00100,
+        0b01010
+    ];
+
+    let ArmsUp = [
+        0b00100,
+        0b01010,
+        0b00100,
+        0b10101,
+        0b01110,
+        0b00100,
+        0b00100,
+        0b01010
+    ];
+
+    let Degree = [
+        0,
+        0b10110,
+        0b01001,
+        0b01000,
+        0b01000,
+        0b01001,
+        0b00110,
+        0,
+    ]
+
+    let Empty = [
+        0b11011,
+        0b10111,
+        0b10111,
+        0b11011,
+        0b11101,
+        0b11110,
+        0b11101,
+        0b11011,
+    ]
+    /**
+         * 测试造字程序
+          */
+    //% blockId="test" block="Test "
+    //% weight=100 blockGap=8
+    export function test() {
+        CreateCustomChar(0, Heart);
+
+        ShowString("test", 0, 0)
+
+        dat(0)
     }
 
-    function i2cread(addr: number, reg: number) {
-        pins.i2cWriteNumber(addr, reg, NumberFormat.UInt8BE);
-        let val = pins.i2cReadNumber(addr, NumberFormat.UInt8BE);
-        return val;
-}
-    function setCursor(col:number,  row:number)  {
-        col = (row == 0 ? col | 0x80 : col | 0xc0);
-        cmd(col)
+    //% blockId="I2C_LCD1620_CustomChar_Conv" block="自定义字符 %c"
+    export function CustomCharConv(c: CustomChar): number[] {
+        switch (c) {
+            case CustomChar.ArmsDown: return ArmsDown;
+            case CustomChar.ArmsUp: return ArmsUp;
+            case CustomChar.Degree: return Degree;
+            case CustomChar.Heart: return Heart;
+            case CustomChar.Sad: return Sad;
+            case CustomChar.Smile: return Smile;
+            case CustomChar.HeartSmall: return HeartSmall;
+        }
+        return Empty;
     }
+
 
     /**
      * 初始化 LCD, 设置 I2C 地址。根据芯片不同地址有两种，LCM1602-14 是62(0x3E)。
      * @param address is i2c address for LCD, eg: 62 (0x3E)
      */
-    //% blockId="I2C_LCD1620_SET_ADDRESS" block="初始化液晶，I2C 地址 %addr"
+    //% blockId="LcdInitx" block="初始化液晶，I2C 地址 %address"
     //% weight=100 blockGap=8
     export function LcdInit(address: number) {
         i2cAddr = address
-        BK = 8
-        RS = 0
-	    basic.pause(50)
+        basic.pause(50)
         cmd(0x28)       // set 4bit mode
+        basic.pause(5)
         cmd(0x28)       // set 4bit mode
         basic.pause(1)
         cmd(0x0C)
         cmd(0x06)
-        basic.pause(20)
         cmd(0x01)       // clear wait more then 2ms
-	    basic.pause(20)
+        basic.pause(5)
     }
 
     /**
@@ -76,7 +163,7 @@ namespace I2C_LCD1602 {
      * @param x is LCD column position, eg: 0
      * @param y is LCD row position, eg: 0
      */
-    //% blockId="I2C_LCD1620_SHOW_NUMBER" block="显示数字 %n|位置 x %x|y %y"
+    //% blockId="I2C_LCD1620_SHOW_NUMBER" block="显示 数字 %n|位置 x %x|y %y"
     //% weight=90 blockGap=8
     //% x.min=0 x.max=15
     //% y.min=0 y.max=1
@@ -87,11 +174,11 @@ namespace I2C_LCD1602 {
 
     /**
      * 在液晶的指定位置显示字符串
-     * @param s is string will be show, eg: "Hello World!"
+     * @param s is string will be show, eg: "Hello micro:bit!"
      * @param x is LCD column position, [0 - 15], eg: 0
      * @param y is LCD row position, [0 - 1], eg: 0
      */
-    //% blockId="I2C_LCD1620_SHOW_STRING" block="显示字符串 %s|位置 x %x|y %y"
+    //% blockId="I2C_LCD1620_SHOW_STRING" block="显示 字符串 %s|位置 x %x|y %y"
     //% weight=90 blockGap=8
     //% x.min=0 x.max=15
     //% y.min=0 y.max=1
@@ -128,28 +215,77 @@ namespace I2C_LCD1602 {
     //% weight=75 blockGap=8
     export function clear(): void {
         cmd(0x01)
-        basic.pause(10)
+        basic.pause(5)
+    }
+
+
+    /**
+     * 造字 1602一共可以造8个5*8的自定义字符
+     * @param code is font code, eg: 0
+     * @param fontList is 8 byte list
+     */
+    //% blockId="I2C_LCD1620_Create_CustomChar" block="自定义字符 %code |编码 %fontList=CustomCharConv"
+    //% code.min=0 code.max=7
+    //% weight=100 blockGap=8
+    export function CreateCustomChar(code: number, fontList: number[]) {
+        cmd(0x40 | (code & 0x7) << 3);
+        let buf = pins.createBuffer(9);
+        buf[0] = 0x40
+        for (let i = 0; i < 8; i++) {
+            buf[i + 1] = fontList[i];
+        }
+        pins.i2cWriteBuffer(i2cAddr, buf);
+        basic.pause(5)
     }
 
     /**
-     * 打开液晶的背光
+     * 在液晶的指定位置显示自定义字符
+     * @param s is string will be show, [0 - 7], eg: 0
+     * @param x is LCD column position, [0 - 15], eg: 0
+     * @param y is LCD row position, [0 - 1], eg: 0
      */
-    //% blockId="I2C_LCD1620_BACKLIGHT_ON" block="打开液晶背光"
-    //% weight=70 blockGap=8
-    //export function BacklightOn(): void {
-    //    //BK = 8
-    //   dat(9)
-    //}
+    //% blockId="I2C_LCD1620_SHOW_STRING_MY" block="显示 自定义字符 %s|位置 x %x|y %y"
+    //% weight=90 blockGap=8
+    //% s.min=0 s.max=7
+    //% x.min=0 x.max=15
+    //% y.min=0 y.max=1
+    export function ShowString_MY(s: number, x: number, y: number): void {
+        setCursor(x, y);
+        dat(s)
+    }
+    // 设置光标位置
+    function setCursor(col: number, row: number) {
+        col = (row == 0 ? col | 0x80 : col | 0xc0);
+        cmd(col)
+    }
+    // send command
+    function cmd(d: number) {
+        pins.i2cWriteNumber(i2cAddr, 0x8000 | d, 9)
+        //basic.pause(1)
+    }
+    // send data
+    function dat(d: number) {
+        pins.i2cWriteNumber(i2cAddr, 0x4000 | d, 9)
+        //basic.pause(1)
+    }
+    // 从robotbit找来的代码，看上去更科学一些。。。。
+    function i2cwrite(addr: number, reg: number, value: number) {
+        let buf = pins.createBuffer(2)
+        buf[0] = reg
+        buf[1] = value
+        pins.i2cWriteBuffer(addr, buf)
+    }
 
-    /**
-     * 关闭液晶的背光
-     */
-    //% blockId="I2C_LCD1620_BACKLIGHT_OFF" block="关闭液晶背光"
-    //% weight=70 blockGap=8
-    //export function BacklightOff(): void {
-    //    //BK = 0
-    //   dat(1)
-    //}
-    
+    function i2ccmd(addr: number, value: number) {
+        let buf = pins.createBuffer(1)
+        buf[0] = value
+        pins.i2cWriteBuffer(addr, buf)
+    }
+
+    function i2cread(addr: number, reg: number) {
+        pins.i2cWriteNumber(addr, reg, NumberFormat.UInt8BE);
+        let val = pins.i2cReadNumber(addr, NumberFormat.UInt8BE);
+        return val;
+    }
 
 }
--- test.ts
+++ test.ts
@@ -1,11 +1,26 @@
-let item = 0
-I2C液晶1602.液晶初始化(63)
-I2C液晶1602.显示字符串("Hello", 0, 0)
-basic.pause(1000)
-I2C液晶1602.清除液晶()
-item = 0
-basic.forever(() => {
-    item += 1
-    I2C液晶1602.显示数字(item, 0, 0)
-    basic.pause(1000)
+
+let count = 0
+input.onButtonPressed(Button.B, () => {
+    for (let i = 0; i < 6; i++) {
+        if (count % 2 == 1) {
+            I2C_LCD1602_14.ShowString_MY(0, 2, 0)
+            basic.pause(1200)
+        } else {
+            I2C_LCD1602_14.ShowString_MY(1, 2, 0)
+            basic.pause(300)
+        }
+        count += 1
+    }
 })
+input.onButtonPressed(Button.A, () => {
+    I2C_LCD1602_14.CreateCustomChar(3, [0, 10, 21, 17, 17, 10, 4, 0])
+    I2C_LCD1602_14.ShowString_MY(3, 2, 0)
+})
+I2C_LCD1602_14.LcdInit(62)
+I2C_LCD1602_14.ShowString("I   micro:bit!", 0, 0)
+I2C_LCD1602_14.CreateCustomChar(0, I2C_LCD1602_14.CustomCharConv(I2C_LCD1602_14.CustomChar.Heart))
+I2C_LCD1602_14.CreateCustomChar(1, I2C_LCD1602_14.CustomCharConv(I2C_LCD1602_14.CustomChar.HeartSmall))
+I2C_LCD1602_14.ShowString_MY(0, 2, 0)
+count = 0
+I2C_LCD1602_14.ShowString("No:", 0, 1)
+I2C_LCD1602_14.ShowNumber(control.deviceSerialNumber(), 3, 1)
